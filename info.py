255d271aa485ca974d3bf77d731fcabdc5f57701
Bot Devloper <telegrambotmaking12@gmail.com>
2025-09-06 16:49:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,10 +29,10 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 # ForceSub Channel & Log Channels
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002177887107'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', ''))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '')) 
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ''))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '0'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '0'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0')) 
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
 
 # MongoDB 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rathv:rathv@rathv.mxyrgyb.mongodb.net/?retryWrites=true&w=majority&appName=rathv")
